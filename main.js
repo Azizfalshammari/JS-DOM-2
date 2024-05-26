4dee7582d36c1426e71a69bc49c52b975084d855
Azizfalshammari <azizfalshammari.com>
2024-05-26 20:33:44 +0300

    colorRandomPosit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+function change()  {
+    let square1 = document.getElementsByClassName('square')[0];
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    square1.style.backgroundColor = '#' +randomColor;
+    // square1.style.top= Math.floor(Math.random() * 101); +'px'
+    console.log(square1.style.top= Math.floor(Math.random() * 1000) +'px')
+    
+    console.log(square1.style.left= Math.floor(Math.random() * 101) +'px')
+    console.log('change is good')
+
+};
+
+
+ setInterval(change, 3000);
+
+
+
