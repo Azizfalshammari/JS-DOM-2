c2d485c1159a8a38c6e8ee8654ac36501adf2c3b
Azizfalshammari <azizfalshammari.com>
2024-05-26 20:39:15 +0300

    sadadssad

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ function change()  {
     let square1 = document.getElementsByClassName('square')[0];
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
     square1.style.backgroundColor = '#' +randomColor;
-    // square1.style.top= Math.floor(Math.random() * 101); +'px'
-    console.log(square1.style.top= Math.floor(Math.random() * 200) +'px')
     
-    console.log(square1.style.left= Math.floor(Math.random() * 200) +'px')
+    console.log(square1.style.top= Math.floor(Math.random() * 101) +'px')
+    console.log(square1.style.left= Math.floor(Math.random() * 101) +'px')
     console.log('change is good')
 };
 setInterval(change, 3000);
